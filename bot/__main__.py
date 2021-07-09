983c17991458a3204ccaa8392ff5033f437fa69a
superman-enamy <47158027+superman-enamy@users.noreply.github.com>
2021-07-10 04:18:10 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,6 @@
 from os.path import join as os_path_join
-from pyrogram import Client, Message, MessageHandler, Filters, CallbackQueryHandler
+from pyrogram import filters, Client
+from pyrogram.types import Message, MessageHandler, Filters, CallbackQueryHandler
 from bot import CONFIG, COMMAND, LOCAL, LOGGER, STATUS
 from bot.handlers import *
 import asyncio
